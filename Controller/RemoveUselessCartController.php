550aaadb3ad7d6a00e29a2afaf6df69d9a2b2440
Etienne PERRIERE <eperriere@openstudio.fr>
2015-08-31 17:11:05 +0200

    Remove carts created before selected date

--- Controller/RemoveUselessCartController.php
+++ Controller/RemoveUselessCartController.php
@@ -2,9 +2,12 @@
 
 namespace RemoveUselessCart\Controller;
 
+use Propel\Runtime\ActiveQuery\Criteria;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use \Thelia\Controller\Admin\BaseAdminController;
 use Thelia\Core\Security\AccessManager;
 use Thelia\Core\Security\Resource\AdminResources;
+use Thelia\Model\CartQuery;
 
 class RemoveUselessCartController extends BaseAdminController
 {
@@ -15,5 +18,35 @@ class RemoveUselessCartController extends BaseAdminController
         }
 
         return $this->render("removeuselesscart-configuration", []);
+    }
+
+    public function removeAction()
+    {
+        if (null !== $response = $this->checkAuth(array(AdminResources::MODULE), 'RemoveUselessCart', AccessManager::DELETE)) {
+            return $response;
+        }
+
+        // Validate form and get data
+        $form = $this->createForm('removeuselesscart_form');
+
+        try {
+            $startDate = $this->validateForm($form, 'POST')->getData()['start_date'];
+
+            CartQuery::create()
+                ->filterByUpdatedAt($startDate, Criteria::LESS_EQUAL)
+                ->delete();
+
+            // Redirect
+            return new RedirectResponse($form->getSuccessUrl());
+        } catch (\Exception $e) {
+            $this->setupFormErrorContext(
+                null,
+                $e->getMessage(),
+                $form
+            );
+
+            return $this->render('removeuselesscart-configuration', []);
+        }
+
     }
 }
---
+++ Form/RemoveUselessCartForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace RemoveUselessCart\Form;
+
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\ExecutionContextInterface;
+use Thelia\Core\Translation\Translator;
+use Thelia\Form\BaseForm;
+
+/**
+ * Class RemoveUselessCartForm
+ * @package RemoveUselessCart\Form
+ * @author Etienne Perriere - OpenStudio <eperriere@openstudio.fr>
+ */
+class RemoveUselessCartForm extends BaseForm
+{
+    // The date format for the start date
+    const PHP_DATE_FORMAT = "Y-m-d H:i:s";
+    const MOMENT_JS_DATE_FORMAT = "YYYY-MM-DD HH:mm:ss";
+
+    /**
+     * @return null
+     */
+    protected function buildForm()
+    {
+        $this->formBuilder
+            ->add('start_date', 'text',
+                [
+                    'label' => 'Remove all carts after this date',
+                    'required'    => true,
+                    'constraints' => [
+                        new Constraints\NotBlank(),
+                        new Constraints\Callback([
+                            "methods" => [
+                                [ $this, "checkDate" ],
+                            ],
+                        ])
+                    ]
+                ]
+            );
+    }
+
+    /**
+     * Validate a date entered with the current edition Language date format.
+     *
+     * @param string                    $value
+     * @param ExecutionContextInterface $context
+     */
+    public function checkDate($value, ExecutionContextInterface $context)
+    {
+        $format = self::PHP_DATE_FORMAT;
+
+        if (! empty($value) && false === \DateTime::createFromFormat($format, $value)) {
+            $context->addViolation(Translator::getInstance()->trans("Date '%date' is invalid, please enter a valid date using %fmt format", [
+                '%fmt' => self::MOMENT_JS_DATE_FORMAT,
+                '%date' => $value
+            ]));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'removeuselesscart_form';
+    }
+}
