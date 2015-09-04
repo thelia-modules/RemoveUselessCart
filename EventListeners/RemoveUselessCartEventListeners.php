d74e990eef34f555ad4b4b74e343f732cdeccb67
Etienne PERRIERE <eperriere@openstudio.fr>
2015-09-04 16:00:41 +0200

    Use direct SQL query to avoid memory limit and timeout. Change a translation.

--- EventListeners/RemoveUselessCartEventListeners.php
+++ EventListeners/RemoveUselessCartEventListeners.php
@@ -3,9 +3,12 @@
 namespace RemoveUselessCart\EventListeners;
 
 use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\Propel;
 use RemoveUselessCart\Event\RemoveUselessCartEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Thelia\Install\Database;
 use Thelia\Model\CartQuery;
+use Thelia\Model\Map\ModuleTableMap;
 
 /**
  * Class RemoveUselessCartEventListeners
@@ -30,43 +33,32 @@ class RemoveUselessCartEventListeners implements EventSubscriberInterface
     /**
      * Propel does not support DELETE with LEFT JOIN, so the function gets
      * carts to remove by range and removes them until there are no more to delete.
+     * Do 5 iterations to avoid server timeout
      *
      * @param RemoveUselessCartEvent $event
      * @throws \Propel\Runtime\Exception\PropelException
      */
     public function remove(RemoveUselessCartEvent $event)
     {
-        $totalRemovedCarts = 0;
+        $startDate = $event->getStartDate();
 
-        do {
-            // Create query filtered by date
-            $findCartQuery = CartQuery::create()
-                ->filterByUpdatedAt($event->getStartDate(), Criteria::LESS_EQUAL);
+        // If carts with products must not be removed
+        if (!$event->getRemoveAll()) {
+            $sqlRemoveCarts =
+                "DELETE `cart` FROM `cart` LEFT JOIN `cart_item` ON ( `cart`.`ID` = `cart_item`.`CART_ID` ) WHERE `cart`.`UPDATED_AT` <= :startDate AND `cart_item`.`QUANTITY` IS NULL ";
+        } else {
+            $sqlRemoveCarts =
+                "DELETE `cart` FROM `cart` WHERE `cart`.`UPDATED_AT` <= :startDate";
+        }
 
-            // If carts with products must not be removed
-            if (!$event->getRemoveAll()) {
-                $findCartQuery
-                    ->leftJoinCartItem()
-                    ->where('cart_item.QUANTITY IS NULL');
-            }
+        // Get database connection
+        $con = Propel::getWriteConnection(ModuleTableMap::DATABASE_NAME);
+        $database = new Database($con);
 
-            // Get an array of cart IDs to remove
-            $cartList = $findCartQuery
-                ->select('ID')
-                ->limit(2000)
-                ->find()
-                ->toArray();
+        // Execute query
+        $stmtRemoveCarts = $database->execute($sqlRemoveCarts, [':startDate' => $startDate]);
 
-            // Remove carts
-            CartQuery::create()
-                ->filterById($cartList)
-                ->delete();
-
-            // Get number of removed carts
-            $totalRemovedCarts += count($cartList);
-        } while (!empty($cartList));
-
-        // Set number of removed carts
-        $event->setRemovedCarts($totalRemovedCarts);
+        // Fill event with number of removed carts
+        $event->setRemovedCarts($stmtRemoveCarts->rowCount());
     }
 }
--- I18n/backOffice/default/en_US.php
+++ I18n/backOffice/default/en_US.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several parts. Please wait until success or error message is displayed.' => '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several parts. Please wait until success or error message is displayed.',
+    '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.' => '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.',
     'OK' => 'OK',
     'Remove useless carts' => 'Remove useless carts',
 );
