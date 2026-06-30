f207c0c29b23e6d8b618c4957eb6c970ac23e182
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-30 12:47:43 +0200

    fix(i18n): add French translations for remove useless cart labels

--- I18n/backOffice/default/en_US.php
+++ I18n/backOffice/default/en_US.php
@@ -4,4 +4,6 @@ return array(
     '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.' => '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.',
     'OK' => 'OK',
     'Remove useless carts' => 'Remove useless carts',
+    'Warning: this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.' => 'Warning: this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.',
+    'Format: YYYY-MM-DD HH:mm:ss' => 'Format: YYYY-MM-DD HH:mm:ss',
 );
--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -4,4 +4,6 @@ return array(
     '<b>Warning :</b> this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.' => '<b>Attention :</b> cette opération peut prendre du temps avec de grosses bases de données. Vous devriez l\'effectuer en plusieurs fois. Merci de patienter jusqu\'à l\'apparition du message de succès ou d\'erreur.',
     'OK' => 'OK',
     'Remove useless carts' => 'Supprimer les paniers inutiles',
+    'Warning: this operation may take a while for huge databases. You should do it in several times. Please wait until success or error message is displayed.' => 'Attention : cette opération peut prendre du temps avec de grosses bases de données. Effectuez-la en plusieurs fois. Merci de patienter jusqu\'à l\'apparition du message de succès ou d\'erreur.',
+    'Format: YYYY-MM-DD HH:mm:ss' => 'Format : AAAA-MM-JJ HH:mm:ss',
 );
